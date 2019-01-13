a5bdc9dd12d0fd77894838a6a504c7976b64be81
Vitalii Kulyk <vitcool@ukr.net>
2019-01-13 15:56:16 +0200

    FC-2: image and button are added

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
       ),
-    );
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                  onPressed: () {
+                    
+                  }, child: Text('Add product'))),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/food.jpeg'),
+                Text('Food Paradise')
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
