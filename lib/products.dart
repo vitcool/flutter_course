66c9ef497f46cfbb24c7a933781bb3f5515fa668
Vitalii Kulyk <vitcool@ukr.net>
2019-01-16 23:10:41 +0200

    FC-6: dynamic list is added

--- lib/products.dart
+++ lib/products.dart
@@ -5,18 +5,22 @@ class Products extends StatelessWidget {
 
   Products([this.products = const []]);
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpeg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: products
-            .map((element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpeg'),
-                      Text(element)
-                    ],
-                  ),
-                ))
-            .toList());
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
   }
 }
