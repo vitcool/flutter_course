a3d0287bfefe98b1d96e778a1413cca5c0f9ce87
Vitalii Kulyk <vitcool@ukr.net>
2019-01-28 09:11:57 +0200

    FC-17: font is added + products page styles are improved

--- lib/products.dart
+++ lib/products.dart
@@ -10,7 +10,22 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(products[index]['image']),
-          Text(products[index]['title']),
+          Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+              Text(
+                products[index]['title'],
+                style: TextStyle(
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Oswald'),
+              ),
+              SizedBox(width: 8.0,),
+              Text(products[index]['price'].toString())
+            ]),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
