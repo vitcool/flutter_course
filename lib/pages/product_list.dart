f8d0abd3d1c3cb2933b9a05a169693478fa4f969
Vitalii Kulyk <vitcool@ukr.net>
2019-02-04 08:56:20 +0200

    FC-24: products list item is improved

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -12,19 +12,28 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (BuildContext context) {
-                return ProductEditPage(
-                    product: products[index], updateProduct: updateProduct, productIndex: index);
-              }));
-            },
+        return Column(children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(products[index]['image']),
+            ),
+            title: Text(products[index]['title']),
+            subtitle: Text('\$${products[index]['price'].toString()}'),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return ProductEditPage(
+                      product: products[index],
+                      updateProduct: updateProduct,
+                      productIndex: index);
+                }));
+              },
+            ),
           ),
-        );
+          Divider()
+        ]);
       },
       itemCount: products.length,
     );
